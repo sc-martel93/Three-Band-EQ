5c4a846246794cbefc5c7c44fbf7345833bc3fa8
sc-martel93 <80806216+sc-martel93@users.noreply.github.com>
2023-04-24 13:17:12 -0400

    Fix peak parameter values

--- Source/PluginProcessor.cpp
+++ Source/PluginProcessor.cpp
@@ -444,12 +444,12 @@ juce::AudioProcessorValueTreeState::ParameterLayout SimpleEQAudioProcessor::crea
 
     layout.add(std::make_unique<juce::AudioParameterFloat>("Peak Gain",
                                                             "Peak Gain",
-                                                            juce::NormalisableRange<float>(-24.f, 24.f, 0.5f, 0.25f),
+                                                            juce::NormalisableRange<float>(-24.f, 24.f, 0.5f, 1.f),
                                                             0.0f));
 
     layout.add(std::make_unique<juce::AudioParameterFloat>("Peak Quality",
                                                             "Peak Quality",
-                                                            juce::NormalisableRange<float>(0.1f, 10.f, 0.05f, 0.25f),
+                                                            juce::NormalisableRange<float>(0.1f, 10.f, 0.05f, 1.f),
                                                             1.f));
 
     juce::StringArray slopeChoices;
