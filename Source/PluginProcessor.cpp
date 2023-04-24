ea70057ba28b42675fb9c4608922c2d41f444fd1
sc-martel93 <80806216+sc-martel93@users.noreply.github.com>
2023-04-24 17:05:02 -0400

    Added sliders to GUI

--- Source/PluginProcessor.cpp
+++ Source/PluginProcessor.cpp
@@ -188,9 +188,9 @@ bool SimpleEQAudioProcessor::hasEditor() const
 
 juce::AudioProcessorEditor* SimpleEQAudioProcessor::createEditor()
 {
-    // return new SimpleEQAudioProcessorEditor (*this);
+    return new SimpleEQAudioProcessorEditor (*this);
 
-    return new juce::GenericAudioProcessorEditor(*this);
+    // return new juce::GenericAudioProcessorEditor(*this);
 }
 
 //==============================================================================
