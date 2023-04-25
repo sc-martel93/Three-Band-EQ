09f45993bb2336a1b23b83e6a437a0aebf03a8dc
sc-martel93 <80806216+sc-martel93@users.noreply.github.com>
2023-04-24 23:23:05 -0400

    Response curve now shows cut filter response

--- Source/PluginEditor.cpp
+++ Source/PluginEditor.cpp
@@ -66,6 +66,12 @@ void SimpleEQAudioProcessorEditor::timerCallback()
         auto peakCoefficients = makePeakFilter(chainSettings, audioProcessor.getSampleRate());
         updateCoefficients(monoChain.get<ChainPositions::Peak>().coefficients, peakCoefficients);
 
+        auto lowCutCoefficients = makeLowCutFilter(chainSettings, audioProcessor.getSampleRate());
+        auto highCutCoefficients = makeHighCutFilter(chainSettings, audioProcessor.getSampleRate());
+
+        updateCutFilter(monoChain.get<ChainPositions::LowCut>(), lowCutCoefficients, chainSettings.lowCutSlope);
+        updateCutFilter(monoChain.get<ChainPositions::HighCut>(), highCutCoefficients, chainSettings.highCutSlope);
+
         // Signal a repaint
         repaint();
     }
