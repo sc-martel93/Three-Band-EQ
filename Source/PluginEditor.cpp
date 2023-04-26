9d9b6b53f847d19a843f87592d8dfc24b590f9cb
sc-martel93 <80806216+sc-martel93@users.noreply.github.com>
2023-04-26 15:33:08 -0400

    Added frequency lables

--- Source/PluginEditor.cpp
+++ Source/PluginEditor.cpp
@@ -335,9 +335,9 @@ void ResponseCurveComponent::resized()
     // Draw vertical frequencies
     Array<float> freqs
     {
-        20, 30, 40, 50, 100,
-        200, 300, 400, 500, 1000,
-        2000, 3000, 4000, 5000, 10000,
+        20, 50, 100,
+        200, 500, 1000,
+        2000, 5000, 10000,
         20000
     };
 
@@ -376,6 +376,43 @@ void ResponseCurveComponent::resized()
         g.setColour(gDb == 0.f ? Colour(0u, 172u, 1u) : Colours::darkgrey);
         g.drawHorizontalLine(y, left, right);
     }
+
+    // Render response grid labels
+    g.setColour(Colours::lightgrey);
+    const int fontHeight = 10;
+    g.setFont(fontHeight);
+
+    // Loop through freqs and label text above each line
+    for (int i = 0; i < freqs.size(); ++i)
+    {
+        auto f = freqs[i];
+        auto x = xs[i];
+        
+        // Add kHz label is needed
+        bool addK = false;
+        String str;
+        if (f > 999.f)
+        {
+            addK = true;
+            f /= 1000.f;
+        }
+
+        str << f;
+        if (addK)
+            str << "k";
+        str << "Hz";
+        
+        // Create background box for label
+        auto textWidth = g.getCurrentFont().getStringWidth(str);
+
+        Rectangle<int> r;
+        r.setSize(textWidth, fontHeight);
+        r.setCentre(x, 0);
+        r.setY(1);
+
+        g.drawFittedText(str, r, juce::Justification::centred, 1);
+    }
+
 }
 
 juce::Rectangle<int> ResponseCurveComponent::getRenderArea()
